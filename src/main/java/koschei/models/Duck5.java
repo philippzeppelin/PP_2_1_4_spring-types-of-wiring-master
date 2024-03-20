85f9188425ed88e1a94c03f2585fd5618bb67cb6
Philipp Zeppelin <philippzeppelin>
2024-03-21 00:14:31 +0300

    Finished the task 2.1.4

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -4,9 +4,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+    private Egg6 egg;
+
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
+    }
+
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
     }
 }
